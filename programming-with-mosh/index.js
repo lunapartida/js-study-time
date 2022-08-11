9984340905cd86d9ab5e14d1dd3b73ddd7ac1d07
Luna Partida <lunapartida93@icloud.com>
2022-08-11 14:39:11 -0700

    first commit

---
+++ programming-with-mosh/index.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+let name = "Mosh";
+console.log(name);
+
+// Cannot be a reserved keyboard
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = "Mosh";
+let lastName = "Partida";
+
+const intrestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
