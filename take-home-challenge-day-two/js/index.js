83fc599845a2d897d8ef8f1bcb4fa5cb3d9d541d
Luna Partida <lunapartida93@icloud.com>
2022-08-15 17:25:35 -0700

    day two take home challenge

---
+++ take-home-challenge-day-two/js/index.js
@@ -0,0 +1,11 @@
+let text = 'Supercalifragilisticexpialidocious';
+let length = text.length;
+console.log(text);
+console.log(length);
+
+const w = BigInt(15);
+const x = 15n;
+
+console.log(w + x);
+
+console.log(5);
